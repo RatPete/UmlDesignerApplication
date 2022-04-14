ab59199019b851ba9d18ed57df83a9b65a1685e0
ratpete <perger.peter.99@gmail.com>
2022-04-14 16:11:44 +0200

    MovementData classes added

---
+++ WpfDiagramDesigner/WpfDiagramDesigner/Source/PRL/ViewModel/EdgeAnimationValues.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace WpfDiagramDesigner.Source.PRL.ViewModel
+{
+    class EdgeAnimationValues
+    {
+        public List<Point> TargetPosition { get; set; }
+    }
+}
---
+++ WpfDiagramDesigner/WpfDiagramDesigner/Source/PRL/ViewModel/NodeAnimationValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace WpfDiagramDesigner.Source.PRL.ViewModel
+{
+    public class NodeAnimationValues
+    {
+        public Point TargetPosition { get; set; }
+        public Size TargetSize { get; set; }
+    }
+}
